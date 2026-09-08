1e342aeb8595fdd9b2d7b07307029325600d312d
mrmudkicker <lbdtpablo@gmail.com>
2026-09-08 15:04:07 -0300

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,7 +10,11 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-# TODO: Add any logging levels and handlers with app.logger
+app.logger.setLevel(logging.INFO)
+for handler in app.logger.handlers:
+	handler.setFormatter(logging.Formatter(
+		'%(asctime)s %(levelname)s %(name)s: %(message)s'
+	))
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
